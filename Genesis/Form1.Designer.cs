d262a06b3fbdd3c7ebdb246fc9902078cedc470c
Teacher <mozgoed123@mail.ru>
2018-09-20 16:56:09 +0300

    Первая версия
    Создана популяция.
    Популяция разделена на 2 группы по признаку Красоты

---
+++ Genesis/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Genesis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.genBeauty = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(16, 386);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(631, 44);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Пуск эмуляции";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // genBeauty
+            // 
+            this.genBeauty.Location = new System.Drawing.Point(152, 12);
+            this.genBeauty.Name = "genBeauty";
+            this.genBeauty.Size = new System.Drawing.Size(495, 40);
+            this.genBeauty.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ген красоты";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 442);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.genBeauty);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Genesis";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar genBeauty;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Genesis/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Genesis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+
+        void countBirds()
+        {
+            double count = 0;
+            for (int bird = 0; bird < population.Length; bird++)
+            {
+                if (population[bird] == true)
+                    count++;
+            }
+            if (count == 0)
+                genBeauty.Value = 0;
+            else
+                genBeauty.Value = (int) (count / population.Length * 100);
+            Application.DoEvents();
+        }
+
+        bool[] population = new bool[10];
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            countBirds();
+
+
+            for (int bird = 0; bird < population.Length/2; bird++)
+            {
+                population[bird] = true;
+            }
+            countBirds();
+        }
+    }
+}
