f511faff8d805cef3388363135108a816c9c44be
Teacher <mozgoed123@mail.ru>
2018-09-27 12:34:46 +0300

    Реализован один проход размножения и вымирания особей

--- Genesis/Form1.Designer.cs
+++ Genesis/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.genBeauty = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblBeautyPercent = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(16, 386);
+            this.btnStart.Location = new System.Drawing.Point(11, 251);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(631, 44);
+            this.btnStart.Size = new System.Drawing.Size(421, 29);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Пуск эмуляции";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -45,28 +47,42 @@
             // 
             // genBeauty
             // 
-            this.genBeauty.Location = new System.Drawing.Point(152, 12);
+            this.genBeauty.Location = new System.Drawing.Point(101, 8);
+            this.genBeauty.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.genBeauty.Name = "genBeauty";
-            this.genBeauty.Size = new System.Drawing.Size(495, 40);
+            this.genBeauty.Size = new System.Drawing.Size(330, 26);
             this.genBeauty.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ген красоты";
             // 
+            // lblBeautyPercent
+            // 
+            this.lblBeautyPercent.AutoSize = true;
+            this.lblBeautyPercent.Location = new System.Drawing.Point(258, 36);
+            this.lblBeautyPercent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBeautyPercent.Name = "lblBeautyPercent";
+            this.lblBeautyPercent.Size = new System.Drawing.Size(21, 13);
+            this.lblBeautyPercent.TabIndex = 3;
+            this.lblBeautyPercent.Text = "0%";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(659, 442);
+            this.ClientSize = new System.Drawing.Size(439, 287);
+            this.Controls.Add(this.lblBeautyPercent);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.genBeauty);
             this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Genesis";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -80,6 +96,7 @@
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.ProgressBar genBeauty;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBeautyPercent;
     }
 }
 
--- Genesis/Form1.cs
+++ Genesis/Form1.cs
@@ -18,7 +18,7 @@ namespace Genesis
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            
+
         }
 
         /// <summary>
@@ -35,22 +35,71 @@ namespace Genesis
             if (count == 0)
                 genBeauty.Value = 0;
             else
-                genBeauty.Value = (int) (count / population.Length * 100);
+                genBeauty.Value = (int)(count / population.Length * 100);
+            lblBeautyPercent.Text = genBeauty.Value.ToString() + "%";
             Application.DoEvents();
         }
 
-        bool[] population = new bool[10];
+        bool[] population = new bool[100];
 
         private void btnStart_Click(object sender, EventArgs e)
         {
             countBirds();
             MessageBox.Show("Создан массив особей без гена красоты");
-            for (int bird = 0; bird < population.Length/2; bird++)
+            for (int bird = 0; bird < population.Length / 2; bird++)
             {
                 population[bird] = true;
             }
             countBirds();
             MessageBox.Show("Заполнена половина популяции особей с геном красоты");
+
+            //Размножение
+            byte[] byteGeneration = new byte[population.Length * 2];
+            int child = 0;
+            for (int bird = 0; bird < population.Length; bird++)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    if (population[bird] == true)
+                        byteGeneration[child++] = 1;
+                    else
+                        byteGeneration[child++] = 0;
+                }
+            }
+
+            //Вымирание
+            Random rnd = new Random();
+            int count = population.Length;
+            while (count > 0)
+                for (child = 0; child < byteGeneration.Length; child++)
+                {
+                    int percent = rnd.Next(0, 100);
+                    if (byteGeneration[child] == 0)
+                    {
+                        if (percent > 70)
+                        {
+                            byteGeneration[child] = 255;
+                            count--;
+                        }
+                    }
+                    if (byteGeneration[child] == 1)
+                    {
+                        if (percent > 70 + 10)
+                        {
+                            byteGeneration[child] = 255;
+                            count--;
+                        }
+                    }
+                    if (count == 0) break;
+                }
+
+            count = 0;
+            for (child = 0; child < byteGeneration.Length; child++)
+            {
+                if (byteGeneration[child] == 0) population[count++] = false;
+                if (byteGeneration[child] == 1) population[count++] = true;
+            }
+            countBirds();
 
             MessageBox.Show("Эмуляция завершена");
         }
