0deed42017aa3b2d603310713f4bc2611afa1bf5
hukimato <danay2211@gmail.com>
2018-10-11 13:55:18 +0300

    Сломанная версия
    Дописал код для работы кол-ва потомков , базовой выживаемости , бонусной выживаемости . Вроде бы сделал RadioButton , но я не уверен , что правильно . Добавил блокировку / разблокировку кнопок . Если запустить программу и нажать "запустить эмуляцию" , программа вылетает и выдаёт ошибку в 136 строке

--- Genesis/Form1.Designer.cs
+++ Genesis/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.numPopulationBonusSurvival = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numChildrenNumber = new System.Windows.Forms.NumericUpDown();
             this.numBeautyPercent = new System.Windows.Forms.NumericUpDown();
             this.label5 = new System.Windows.Forms.Label();
             this.lblStep = new System.Windows.Forms.Label();
@@ -50,7 +50,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numPopulationSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPopulationSurvival)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPopulationBonusSurvival)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numChildrenNumber)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numBeautyPercent)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numDelay)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numBeautryCount)).BeginInit();
@@ -58,9 +58,10 @@
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(16, 386);
+            this.btnStart.Location = new System.Drawing.Point(11, 251);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(632, 45);
+            this.btnStart.Size = new System.Drawing.Size(421, 29);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Пуск эмуляции";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -68,35 +69,39 @@
             // 
             // genBeauty
             // 
-            this.genBeauty.Location = new System.Drawing.Point(152, 12);
+            this.genBeauty.Location = new System.Drawing.Point(101, 8);
+            this.genBeauty.Margin = new System.Windows.Forms.Padding(2);
             this.genBeauty.Name = "genBeauty";
-            this.genBeauty.Size = new System.Drawing.Size(495, 40);
+            this.genBeauty.Size = new System.Drawing.Size(330, 26);
             this.genBeauty.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.Size = new System.Drawing.Size(71, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Ген красоты";
             // 
             // lblBeautyPercent
             // 
             this.lblBeautyPercent.AutoSize = true;
-            this.lblBeautyPercent.Location = new System.Drawing.Point(387, 55);
+            this.lblBeautyPercent.Location = new System.Drawing.Point(258, 36);
+            this.lblBeautyPercent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBeautyPercent.Name = "lblBeautyPercent";
-            this.lblBeautyPercent.Size = new System.Drawing.Size(32, 20);
+            this.lblBeautyPercent.Size = new System.Drawing.Size(21, 13);
             this.lblBeautyPercent.TabIndex = 3;
             this.lblBeautyPercent.Text = "0%";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 92);
+            this.label2.Location = new System.Drawing.Point(8, 60);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.Size = new System.Drawing.Size(102, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Размер популяции";
             // 
@@ -107,7 +112,8 @@
             0,
             0,
             0});
-            this.numPopulationSize.Location = new System.Drawing.Point(226, 92);
+            this.numPopulationSize.Location = new System.Drawing.Point(151, 60);
+            this.numPopulationSize.Margin = new System.Windows.Forms.Padding(2);
             this.numPopulationSize.Maximum = new decimal(new int[] {
             1000000,
             0,
@@ -119,7 +125,7 @@
             0,
             0});
             this.numPopulationSize.Name = "numPopulationSize";
-            this.numPopulationSize.Size = new System.Drawing.Size(140, 26);
+            this.numPopulationSize.Size = new System.Drawing.Size(93, 20);
             this.numPopulationSize.TabIndex = 5;
             this.numPopulationSize.Value = new decimal(new int[] {
             1000,
@@ -130,15 +136,14 @@
             // 
             // numPopulationSurvival
             // 
-            this.numPopulationSurvival.Location = new System.Drawing.Point(226, 135);
-            this.numPopulationSurvival.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numPopulationSurvival.Location = new System.Drawing.Point(151, 88);
             this.numPopulationSurvival.Minimum = new decimal(new int[] {
             2,
             0,
             0,
             0});
             this.numPopulationSurvival.Name = "numPopulationSurvival";
-            this.numPopulationSurvival.Size = new System.Drawing.Size(180, 26);
+            this.numPopulationSurvival.Size = new System.Drawing.Size(120, 20);
             this.numPopulationSurvival.TabIndex = 6;
             this.numPopulationSurvival.Value = new decimal(new int[] {
             60,
@@ -149,15 +154,14 @@
             // 
             // numPopulationBonusSurvival
             // 
-            this.numPopulationBonusSurvival.Location = new System.Drawing.Point(226, 178);
-            this.numPopulationBonusSurvival.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numPopulationBonusSurvival.Location = new System.Drawing.Point(151, 116);
             this.numPopulationBonusSurvival.Minimum = new decimal(new int[] {
             100,
             0,
             0,
             -2147483648});
             this.numPopulationBonusSurvival.Name = "numPopulationBonusSurvival";
-            this.numPopulationBonusSurvival.Size = new System.Drawing.Size(180, 26);
+            this.numPopulationBonusSurvival.Size = new System.Drawing.Size(120, 20);
             this.numPopulationBonusSurvival.TabIndex = 7;
             this.numPopulationBonusSurvival.Value = new decimal(new int[] {
             5,
@@ -169,36 +173,33 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(10, 138);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(7, 90);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(194, 20);
+            this.label3.Size = new System.Drawing.Size(133, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Базовая выживаемость ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(10, 182);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(7, 118);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(201, 20);
+            this.label4.Size = new System.Drawing.Size(138, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Бонусная выживаемость ";
             // 
-            // numericUpDown1
+            // numChildrenNumber
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(226, 218);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.numericUpDown1.Minimum = new decimal(new int[] {
+            this.numChildrenNumber.Location = new System.Drawing.Point(151, 142);
+            this.numChildrenNumber.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(180, 26);
-            this.numericUpDown1.TabIndex = 10;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.numChildrenNumber.Name = "numChildrenNumber";
+            this.numChildrenNumber.Size = new System.Drawing.Size(120, 20);
+            this.numChildrenNumber.TabIndex = 10;
+            this.numChildrenNumber.Value = new decimal(new int[] {
             4,
             0,
             0,
@@ -207,8 +208,7 @@
             // numBeautyPercent
             // 
             this.numBeautyPercent.DecimalPlaces = 3;
-            this.numBeautyPercent.Location = new System.Drawing.Point(193, 258);
-            this.numBeautyPercent.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numBeautyPercent.Location = new System.Drawing.Point(129, 168);
             this.numBeautyPercent.Maximum = new decimal(new int[] {
             99,
             0,
@@ -220,7 +220,7 @@
             0,
             0});
             this.numBeautyPercent.Name = "numBeautyPercent";
-            this.numBeautyPercent.Size = new System.Drawing.Size(96, 26);
+            this.numBeautyPercent.Size = new System.Drawing.Size(64, 20);
             this.numBeautyPercent.TabIndex = 11;
             this.numBeautyPercent.Value = new decimal(new int[] {
             1,
@@ -231,44 +231,40 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 222);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(8, 144);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(142, 20);
+            this.label5.Size = new System.Drawing.Size(96, 13);
             this.label5.TabIndex = 12;
             this.label5.Text = "Кол-во потомков ";
             // 
             // lblStep
             // 
             this.lblStep.AutoSize = true;
-            this.lblStep.Location = new System.Drawing.Point(475, 55);
-            this.lblStep.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStep.Location = new System.Drawing.Point(317, 36);
             this.lblStep.Name = "lblStep";
-            this.lblStep.Size = new System.Drawing.Size(110, 20);
+            this.lblStep.Size = new System.Drawing.Size(75, 13);
             this.lblStep.TabIndex = 14;
             this.lblStep.Text = "Поколение: 0";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(12, 301);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(8, 196);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(208, 20);
+            this.label7.Size = new System.Drawing.Size(140, 13);
             this.label7.TabIndex = 16;
             this.label7.Text = "Задержка между этапами";
             // 
             // numDelay
             // 
-            this.numDelay.Location = new System.Drawing.Point(226, 297);
-            this.numDelay.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numDelay.Location = new System.Drawing.Point(151, 193);
             this.numDelay.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.numDelay.Name = "numDelay";
-            this.numDelay.Size = new System.Drawing.Size(180, 26);
+            this.numDelay.Size = new System.Drawing.Size(120, 20);
             this.numDelay.TabIndex = 15;
             this.numDelay.Value = new decimal(new int[] {
             500,
@@ -280,9 +276,10 @@
             // 
             this.radGenPercent.AutoSize = true;
             this.radGenPercent.Checked = true;
-            this.radGenPercent.Location = new System.Drawing.Point(16, 258);
+            this.radGenPercent.Location = new System.Drawing.Point(11, 168);
+            this.radGenPercent.Margin = new System.Windows.Forms.Padding(2);
             this.radGenPercent.Name = "radGenPercent";
-            this.radGenPercent.Size = new System.Drawing.Size(170, 24);
+            this.radGenPercent.Size = new System.Drawing.Size(118, 17);
             this.radGenPercent.TabIndex = 17;
             this.radGenPercent.TabStop = true;
             this.radGenPercent.Text = "% особей с геном ";
@@ -292,9 +289,10 @@
             // radGenCount
             // 
             this.radGenCount.AutoSize = true;
-            this.radGenCount.Location = new System.Drawing.Point(296, 258);
+            this.radGenCount.Location = new System.Drawing.Point(197, 168);
+            this.radGenCount.Margin = new System.Windows.Forms.Padding(2);
             this.radGenCount.Name = "radGenCount";
-            this.radGenCount.Size = new System.Drawing.Size(208, 24);
+            this.radGenCount.Size = new System.Drawing.Size(144, 17);
             this.radGenCount.TabIndex = 18;
             this.radGenCount.Text = "Кол-во особей с геном ";
             this.radGenCount.UseVisualStyleBackColor = true;
@@ -302,8 +300,7 @@
             // numBeautryCount
             // 
             this.numBeautryCount.Enabled = false;
-            this.numBeautryCount.Location = new System.Drawing.Point(511, 258);
-            this.numBeautryCount.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numBeautryCount.Location = new System.Drawing.Point(341, 168);
             this.numBeautryCount.Maximum = new decimal(new int[] {
             99,
             0,
@@ -315,7 +312,7 @@
             0,
             0});
             this.numBeautryCount.Name = "numBeautryCount";
-            this.numBeautryCount.Size = new System.Drawing.Size(136, 26);
+            this.numBeautryCount.Size = new System.Drawing.Size(91, 20);
             this.numBeautryCount.TabIndex = 19;
             this.numBeautryCount.Value = new decimal(new int[] {
             1,
@@ -325,9 +322,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(658, 442);
+            this.ClientSize = new System.Drawing.Size(439, 287);
             this.Controls.Add(this.numBeautryCount);
             this.Controls.Add(this.radGenCount);
             this.Controls.Add(this.radGenPercent);
@@ -336,7 +333,7 @@
             this.Controls.Add(this.lblStep);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.numBeautyPercent);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.numChildrenNumber);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numPopulationBonusSurvival);
@@ -348,6 +345,7 @@
             this.Controls.Add(this.genBeauty);
             this.Controls.Add(this.btnStart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -356,7 +354,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.numPopulationSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPopulationSurvival)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numPopulationBonusSurvival)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numChildrenNumber)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numBeautyPercent)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numDelay)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numBeautryCount)).EndInit();
@@ -377,7 +375,7 @@
         private System.Windows.Forms.NumericUpDown numPopulationBonusSurvival;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numChildrenNumber;
         private System.Windows.Forms.NumericUpDown numBeautyPercent;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label lblStep;
--- Genesis/Form1.cs
+++ Genesis/Form1.cs
@@ -55,12 +55,27 @@ namespace Genesis
             btnStart.Text = "Остановить";
             //TODO: блокировать остальные кнопки и внизу разблокировать
             numPopulationSize.Enabled = false;
+            numPopulationSurvival.Enabled = false;
+            numPopulationBonusSurvival.Enabled = false;
+            numChildrenNumber.Enabled = false;
+            numBeautyPercent.Enabled = false;
+            numBeautryCount.Enabled = false;
+            numDelay.Enabled = false;
 
             int populationSize = (int)numPopulationSize.Value;
             population = new bool[populationSize];
             countBirds();
-            decimal BeautyPercent = numBeautyPercent.Value / 100;
-            for (int bird = 0; bird < population.Length * BeautyPercent; bird++)
+            decimal BeautyPercent ;
+            if (radGenPercent.Checked == true)
+            {
+                BeautyPercent = numBeautyPercent.Value / 100;
+            }
+            else
+            {
+             BeautyPercent = numBeautryCount.Value / populationSize; //тут ,чтобы не заморачиваться с кол-вом особей с геном, нашел сколько процентов составляют ососби с геном// 
+            }
+           
+            for (int bird = 0; bird < populationSize * BeautyPercent; bird++)
             {
                 population[bird] = true;
             }
@@ -80,12 +95,12 @@ namespace Genesis
                 }
 
                 //Размножение
-                //TODO: создать интерфейс для количества потомков (2)--
-                byte[] byteGeneration = new byte[population.Length * 2];
+                //TODO: создать интерфейс для количества потомков (2)--(сделал)
+                byte[] byteGeneration = new byte[populationSize * ((int)numChildrenNumber.Value + 1)];
                 int child = 0;
-                for (int bird = 0; bird < population.Length; bird++)
+                for (int bird = 0; bird < populationSize; bird++)
                 {
-                    for (int i = 0; i < 2; i++)//И тут тоже
+                    for (int i = 0; i < (numChildrenNumber.Value + 1); i++)//И тут тоже
                     {
                         if (population[bird] == true)
                             byteGeneration[child++] = 1;
@@ -101,12 +116,12 @@ namespace Genesis
                     for (child = 0; child < byteGeneration.Length; child++)
                     {
                         int percent = rnd.Next(0, 100);
-                        if (byteGeneration[child] == 0 && percent > 70)//TODO: создать интерфейс для базовой выживаемости особи (70)
+                        if (byteGeneration[child] == 0 && percent > numPopulationSurvival.Value )//TODO: создать интерфейс для базовой выживаемости особи (70)(Сделал)
                         {
                             byteGeneration[child] = 255;
                             count--;
                         }
-                        if (byteGeneration[child] == 1 && percent > 70 + 10) //TODO: создать интерфейс бонусной выживаемости красивой особи (10)
+                        if (byteGeneration[child] == 1 && percent > numPopulationSurvival.Value + numPopulationBonusSurvival.Value) //TODO: создать интерфейс бонусной выживаемости красивой особи (10)(Сделал)
                         {
                             byteGeneration[child] = 255;
                             count--;
@@ -118,8 +133,8 @@ namespace Genesis
                 count = 0;
                 for (child = 0; child < byteGeneration.Length; child++)
                 {
-                    if (byteGeneration[child] == 0) population[count++] = false;
-                    if (byteGeneration[child] == 1) population[count++] = true;
+                    if (byteGeneration[child] == 0) { population[count++] = false; }
+                    if (byteGeneration[child] == 1) { population[count++] = true; }
                 }
                 lblStep.Text = "Поколение: " + ++step;
                 countBirds();
@@ -131,6 +146,14 @@ namespace Genesis
 
             btnStart.Text = "Пуск эмуляции";
             numPopulationSize.Enabled = true;
+            numPopulationSurvival.Enabled = true;
+            numPopulationBonusSurvival.Enabled = true;
+            numChildrenNumber.Enabled = true;
+            numBeautyPercent.Enabled = true;
+            numBeautryCount.Enabled = true;
+            numDelay.Enabled = true;
+
+
         }
 
         private void numPopulationSize_Scroll(object sender, ScrollEventArgs e)
@@ -153,7 +176,21 @@ namespace Genesis
 
         private void radGenPercent_CheckedChanged(object sender, EventArgs e)
         {
-            //TODO: при клике по левому RadioButton - выключается правый цифровой блок. И наоборот.
+            //TODO: при клике по левому RadioButton - выключается правый цифровой блок. И наоборот.(вроде сделал)
+            if ( radGenPercent.Checked == true)
+            {
+                radGenCount.Checked = false;
+                numBeautyPercent.Enabled = true;
+                numBeautryCount.Enabled = false;
+            }
+            else
+            {
+                radGenCount.Checked = true;
+                numBeautyPercent.Enabled = false ;
+                numBeautryCount.Enabled = true;
+            }
         }
+
+
     }
 }
